808001b0295dace435df0736ce706cf8f563eec2
Krasnyansky Alexandr <krasnyansky.alexandr@gmail.com>
2023-02-04 23:27:30 +0300

    feat / 15 cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { mongoose } = require('mongoose');
 const bodyParser = require('body-parser');
 const { celebrate, Joi, errors } = require('celebrate');
 const rateLimit = require('express-rate-limit');
+const cors = require('cors');
 const users = require('./routes/users');
 const cards = require('./routes/cards');
 const { createUser, login, logout } = require('./controllers/users');
@@ -12,7 +13,6 @@ const auth = require('./middlewares/auth');
 const error = require('./middlewares/error');
 const { validateRegex } = require('./utils/validateRegex');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { corsPolicy } = require('./middlewares/corsPolicy');
 
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
-app.use(corsPolicy);
+app.use(cors());
 
 app.post(
   '/signin',
