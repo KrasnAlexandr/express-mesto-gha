b1d34762b049ade369950abf9b1aa23f6d9dbf03
Krasnyansky Alexandr <krasnyansky.alexandr@gmail.com>
2023-02-05 21:00:16 +0300

    feat / 15 cors

--- app.js
+++ app.js
@@ -44,6 +44,12 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
 }));
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post(
   '/signin',
   celebrate({
